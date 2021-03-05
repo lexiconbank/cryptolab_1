90358da0cd4fa1b76b1ad4d5450d2c59a8a1e5b6
cjru2 <54086998+cjru2@users.noreply.github.com>
2021-03-05 19:55:09 +0800

    3/5/2021

---
+++ frontend/src/pages/Documentation/Profile.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="documentation">
+
+<!-- markdown -->
+<q-markdown >
+Profile
+===
+
+</q-markdown>
+<!-- /markdown -->
+
+<div class="component q-ma-lg">
+    <div class="form-container">
+        <u-q-profile></u-q-profile>
+    </div>
+</div>
+
+<!-- markdown -->
+<q-markdown >
+
+The __code below__ is for `Profile`
+
+``` html
+{{ code }}
+```
+
+</q-markdown>
+<!-- /markdown -->
+
+
+</div>
+</template>
+
+<script>
+import UQProfile from '../../components/UQProfile'
+
+export default
+{
+    data:() =>
+    ({
+        code: `<div class="component q-ma-lg">
+    <div class="form-container">
+        <u-q-profile></u-q-profile>
+    </div>
+</div>`
+    }),
+    components: { UQProfile },
+    mounted()
+    {
+    },
+    methods: { },
+}
+</script>
